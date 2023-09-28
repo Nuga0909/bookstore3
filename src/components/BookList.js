52ab763dd6e9e4ad01a54befb376d1da89dbcc8f
Adeshina Adenuga <Nuga0909@gmail.com>
2023-09-28 10:01:34 +0100

    refactor booklist.js file

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,44 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Books';
 import Form from './BooksForm';
 
-class BookList extends React.PureComponent {
-  render() {
-    const state = [
-      {
-        key: 0,
-        title: 'Master stroke',
-        author: 'ABBK',
-      },
-      {
-        key: 1,
-        title: 'Mama mia',
-        author: 'ABBK',
-      },
-      {
-        key: 2,
-        title: 'Bongoland',
-        author: 'Gommy',
-      },
-      {
-        key: 3,
-        title: 'Bad guys',
-        author: 'Gommy',
-      },
-    ];
-    return (
-      <>
-        <ul>
-          {state.map((books) => (
-            <li key={books.key}>
-              {' '}
-              <Book title={books.title} author={books.author} />
-            </li>
-          ))}
-        </ul>
-        <Form />
-      </>
-    );
-  }
+function BookList() {
+  const books = useSelector((state) => state.books);
+  return (
+    <>
+      <ul>
+        {books.map((books) => (
+          <li key={books.key}>
+            {' '}
+            <Book title={books.title} author={books.author} />
+          </li>
+        ))}
+      </ul>
+      <Form />
+    </>
+  );
 }
 export default BookList;
