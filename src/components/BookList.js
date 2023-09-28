b485d975d9d5772b3117a9377d30c36c08370850
Adeshina Adenuga <Nuga0909@gmail.com>
2023-09-28 13:50:31 +0100

    change books.js tobook.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Book from './Books';
+import Book from './Book';
 import Form from './BooksForm';
 
 function BookList() {
